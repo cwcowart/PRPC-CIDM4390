ca126c2c4c8205219f45e8e7b10ba08967faa304
cwcowart <coltencowart@gmail.com>
2018-10-28 16:24:06 -0500

    https://sendgrid.com/docs/api-reference/

---
+++ SendGripSMTP/SMTPClient/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using SMTPLib;
+
+namespace SMTPClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SendGrid SMTP = new SendGrid();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ SendGripSMTP/SMTPLib/SendGrid.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using SendGrid.Helpers.Mail;
+using SendGrid;
+using System.Threading.Tasks;
+
+namespace SMTPLib
+{
+    interface ISMTP
+    {
+        Task Execute(msg);
+    }
+
+    public class SendGrid : ISMTP
+    {
+        private static void Main()
+        {
+            Execute().Wait();
+        }
+
+        public async Task Execute()
+        {
+            var apiKey = System.Environment.GetEnvironmentVariable("SENDGRID_API_KEY");
+            var client = new SendGridClient(apiKey);
+            var msg = new SendGridMessage()
+            {
+            var apiKey = Environment.GetEnvironmentVariable("SENDGRID_API_KEY");
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress("test@example.com", "Example User");
+            var subject = "Verification Email";
+            var to = new EmailAddress();
+            var plainTextContent = "Please click on the link";
+            var htmlContent = "<strong>Please click on the link</strong>";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            var response = await client.SendEmailAsync(msg);
+        }
+    }
+}
